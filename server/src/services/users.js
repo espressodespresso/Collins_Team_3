820e211eeaa10db10010074204519b216494ef24
IGNCensor <thenestofficialmail@gmail.com>
2023-02-13 11:07:15 +0000

    Added users service

---
+++ server/src/services/users.js
@@ -0,0 +1,26 @@
+import fetch from "node-fetch";
+
+
+const generateToken = () => {
+    fetch('https://hallam.sci-toolset.com/api/v1/token', {
+        method: 'POST',
+        body: 'grant_type=password&username=' + username + '&password=' + password,
+        headers: {
+
+
+            Authorization: 'Basic ' + clientID + ":" + secret,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then(function (resp){
+        console.log(resp)
+        return resp.json()
+    }).then(function (data){
+        console.log(data)
+        data.cookie('auth',)
+    }).then(function (error){
+        console.log(error)
+    })
+}
+
+
+export {generateToken}
