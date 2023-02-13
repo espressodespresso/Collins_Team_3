7dbc8e880beb65258ee904783856790cfd5b18ec
[CS-4] <[cameron.smith2406@gmail.com]>
2023-02-13 14:30:49 +0000

    route scaffolding

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,8 @@
 import * as dotenv from 'dotenv'
 import app from './server.js'
-import {generateToken} from "./services/users.js";
 
 dotenv.config()
 
 app.listen(process.env.DEV_PORT, () => {
     console.log(`Server started on http://localhost:${process.env.DEV_PORT}`)
 })
-
-generateToken()
