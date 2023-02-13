f1f9b6fc0d171fc36c589e522268870e6ed7ec79
DMSurowy <dm.surowy@gmail.com>
2023-02-13 14:18:23 +0000

    Map and Header

--- client/script.js
+++ client/script.js
@@ -3,3 +3,5 @@ var map = L.map('map').setView([51.505, -0.09], 7);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+
