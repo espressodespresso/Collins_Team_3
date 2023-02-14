ee14026c8f344b8bac7d77d70c1e1fc2c1f4ffe4
larkin1301 <91855449+larkin1301@users.noreply.github.com>
2023-02-14 11:47:38 +0000

    added objects
    im screaming

--- client/script.js
+++ client/script.js
@@ -1,9 +1,52 @@
-var map = L.map('map', {zoomControl: false}).setView([51.505, -0.09], 7);
+var map = L.map('map', {zoomControl: false}).setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.open***REMOVED***reetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.open***REMOVED***reetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
+//var marker = L.marker([51.51, -0.047]).addTo(map);
+
+var object = {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-0.047, 51.51]
+    },
+    "properties": {
+      "name": "help"
+    }
+  };
+
+var workplease = new L.GeoJSON(object).addTo(map);
+
+workplease.setStyle(function(Feature){
+    return {color: red};
+    }
+);
+
+
+
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
+
+
+/*
 function selectShape(sShape){
     switch (sShape){
         case 1:
@@ -20,3 +63,4 @@ function selectShape(sShape){
             break;
     }
 }
+*/
