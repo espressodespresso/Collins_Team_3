adefa43541268b2ce329c29e52dea74f0c9b752c
[CS-4] <[cameron.smith2406@gmail.com]>
2023-02-14 12:49:12 +0000

    Added input validation to login route handler

--- server/src/api/router.js
+++ server/src/api/router.js
@@ -1,8 +1,10 @@
 import {Router} from 'express'
 import {login} from '../services/users.js'
+import {body} from 'express-validator'
+import { handleInputErrors } from '../modules/inputValidation.js'; 
 
 const router = Router();
 
-router.post('/login', login)
+router.post('/login', body('username').isString(), body('password').isString(), handleInputErrors, login)
 
 export default router
