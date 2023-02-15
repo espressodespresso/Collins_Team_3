87f7c016b617768d886d175c7f37f2b947f80d60
IGNCensor <thenestofficialmail@gmail.com>
2023-02-15 14:18:18 +0000

    Added scenes.js

--- server/src/api/dashboard.js
+++ server/src/api/dashboard.js
@@ -1,9 +1,13 @@
 import {json, Router} from 'express'
 import {getMissions, getMission} from "../services/missions.js"
+import {getScene, getScenes} from "../services/scenes.js";
+
 const router = Router()
 
 // Mission routes
 router.get('/missions', getMissions)
 router.get("/mission/:id", getMission)
+router.get("/mission/:mission_id/scene/:scene_id", getScene)
+router.get("/mission/:mission_id/scenes", getScenes)
 
 export default router
