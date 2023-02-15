ef3e3a999dbdfdd637d697d23eaf915fa8a44467
CS-4 <122801411+CS-4@users.noreply.github.com>
2023-02-15 09:06:12 +0000

    Revert "Memcached stuff"

--- server/src/api/dashboard.js
+++ server/src/api/dashboard.js
@@ -2,69 +2,23 @@ import {Router} from 'express'
 import config from "../config/index.js";
 import fetch, {Headers} from "node-fetch";
 import merge from "lodash";
-import memcached from "../db.js";
 const router = Router()
-const auth = "Bearer " + encodeURI(config.accesstoken)
 
-router.get('/missions', async (req, res) => {
-    var missions = await getMissions()
-    res.send(missions)
-    memcached.set("Missions", missions, 10000, await function (err) {
-        console.log(err)
-    })
-
-    memcached.get("Missions", await function(err,data) {
-        console.log(data)
-    })
+router.get('/mission', async (req, res) => {
+    res.send(await getMissions())
     //console.log(await getMissions())
 })
-router.get('/:mission', async (req, res) => {
-    let id = req.params.mission
-    res.send(await getMission(id))
-    console.log("mission id:" + id)
+router.get('/mission/:id', (req, res) => {
+    console.log("mission id:" + req.params.id)
 })
 
-const getMission = async (id) => {
-    memcached.get("Missions", async function (err, data) {
-        console.log(data)
-        if (err) {
-            console.log("API call")
-            try {
-                const getMissionURL = 'https://hallam.sci-toolset.com/discover/api/v1/missionfeed/missions/' + id
-                console.log(getMissionURL)
-                const getMissionResponse = await fetch(getMissionURL, {
-                    method: "GET",
-                    headers: new Headers({
-                        "Content-Type": "application/json",
-                        "Authorization": auth,
-                        "Accept": "*/*"
-                    }),
-                })
-
-                if (getMissionResponse.status === 200) {
-                    var mission = await getMissionResponse.json()
-                    merge.mergeWith(mission, await getMissionFootprint(id))
-                    let geoJSON = {"type":"Mission","geometry":{"type":mission.type,"coordinates":mission.coordinates}
-                        ,"properties":{"name":mission.name,"aircraftTakeOffTime":mission.aircraftTakeOffTime}}
-                    return geoJSON
-                } else {
-                    console.log(statusMessage(getMissionResponse.status))
-                }
-            } catch (e) {
-                console.error(e)
-            }
-        } else {
-            console.log("Getting cached data")
-            return data
-        }
-    })
-}
 
 const getMissions = async () => {
     let responseJSON;
 
     try{
         const getMissionsURL = 'https://hallam.sci-toolset.com/discover/api/v1/missionfeed/missions'
+        const auth = "Bearer " + encodeURI(config.accesstoken)
 
         const getMissionsResponse = await fetch(getMissionsURL, {
             method: "GET",
@@ -80,7 +34,16 @@ const getMissions = async () => {
             let missions = responseJSON.missions
             var footprints = {}
             for(let i=0; i < missions.length; i++) {
-                footprints[i] = await getMissionFootprint(missions[i].id)
+                const getFootprintResponse = await fetch("https://hallam.sci-toolset.com" +
+                "/discover/api/v1/missionfeed/missions/" + missions[i].id + "/footprint", {
+                    method: "GET",
+                    headers: new Headers({
+                        "Content-Type": "application/json",
+                        "Authorization": auth,
+                        "Accept": "*/*"
+                    }),
+                })
+                footprints[i] = await getFootprintResponse.json()
             }
             merge.mergeWith(missions, footprints)
 
@@ -92,6 +55,7 @@ const getMissions = async () => {
                         , "aircraftTakeOffTime": currentMission.aircraftTakeOffTime,}}
             }
 
+
             return geoJSONFormat
         } else {
             return statusMessage(getMissionsResponse.status)
@@ -100,6 +64,8 @@ const getMissions = async () => {
         console.error(e)
     }
 }
+
+
 
 function statusMessage(code) {
     switch (code) {
@@ -114,19 +80,6 @@ function statusMessage(code) {
         default:
             return "?"
     }
-}
-
-async function getMissionFootprint(id) {
-    const getFootprintResponse = await fetch("https://hallam.sci-toolset.com" +
-        "/discover/api/v1/missionfeed/missions/" + id + "/footprint", {
-        method: "GET",
-        headers: new Headers({
-            "Content-Type": "application/json",
-            "Authorization": auth,
-            "Accept": "*/*"
-        }),
-    })
-    return await getFootprintResponse.json()
 }
 
 export default router
