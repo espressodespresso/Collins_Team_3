67906a248023ec5333bfbead556f19291ec49169
CS-4 <122801411+CS-4@users.noreply.github.com>
2023-02-15 09:06:12 +0000

    Revert "Memcached stuff"

--- server/src/db.js
+++
@@ -1,9 +0,0 @@
-import Memcached from "memcached";
-con***REMOVED*** memcached = new Memcached()
-memcached.connect("localho***REMOVED***:11211", function (err, conn) {
-    if(err) {
-        console.log("Error when e***REMOVED***ablishing memcached connection")
-    }
-})
-
-export default memcached
