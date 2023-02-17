ba62270c49b8c0f483297ac97ffc120785494ae1
c1023738 <c1023738@my.shu.ac.uk>
2023-02-17 09:28:14 +0000

    It worked for me
    sry if i break main

---
+++ client/leaflet-search-master/examples/data/restaurant.geojson.js
@@ -0,0 +1,728 @@
+var re***REMOVED***aurant = {
+  "type": "FeatureCollection",
+  "generator": "overpass-turbo",
+  "copyright": "The data included in this document is from www.open***REMOVED***reetmap.org. The data is made available under ODbL.",
+  "time***REMOVED***amp": "2015-08-08T19:02:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "id": "node/252601050",
+      "properties": {
+        "@id": "node/252601050",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "chinese",
+        "name": "Hang Zhou"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4994694,
+          41.8957882
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1479450328",
+      "properties": {
+        "@id": "node/1479450328",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "E***REMOVED*** E***REMOVED*** E***REMOVED*** !",
+        "website": "www.anticapizzeriaricciroma.com"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4926565,
+          41.8989567
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1530298843",
+      "properties": {
+        "@id": "node/1530298843",
+        "amenity": "re***REMOVED***aurant",
+        "name": "Ho***REMOVED***aria Isidoro Al Colosseo"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4986415,
+          41.8888149
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1536870110",
+      "properties": {
+        "@id": "node/1536870110",
+        "addr:housenumber": "40",
+        "addr:***REMOVED***reet": "Largo Corrado Ricci",
+        "amenity": "re***REMOVED***aurant",
+        "name": "Angelino ai Fori",
+        "wheelchair": "limited"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.488007,
+          41.8930475
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1536870121",
+      "properties": {
+        "@id": "node/1536870121",
+        "addr:housenumber": "37",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "pizza",
+        "name": "Pizzeria Imperiale",
+        "wheelchair": "limited"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4882146,
+          41.8931487
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1542760517",
+      "properties": {
+        "@id": "node/1542760517",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "Made in Sud"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4956215,
+          41.8895889
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1589901714",
+      "properties": {
+        "@id": "node/1589901714",
+        "amenity": "re***REMOVED***aurant",
+        "name": "Kabir Fa***REMOVED*** Food"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.5046274,
+          41.8956069
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1609106628",
+      "properties": {
+        "@id": "node/1609106628",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "Pretoriana"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.5043739,
+          41.9050278
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1660404815",
+      "properties": {
+        "@id": "node/1660404815",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "regional",
+        "name": "Al Vino al Vino",
+        "re***REMOVED***aurant:type": "enoteca"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4903691,
+          41.8955158
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1660404843",
+      "properties": {
+        "@id": "node/1660404843",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "regional",
+        "name": "Taverna Romana"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4894514,
+          41.8940973
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1660412593",
+      "properties": {
+        "@id": "node/1660412593",
+        "addr:housenumber": "313",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "regional",
+        "email": "cavour313@libero.it",
+        "fax": "+39 06 6785496",
+        "name": "Enoteca Cavour 313",
+        "operator": "Enoteca Cavour 313 s.r.l.",
+        "phone": "+39 06 6785496",
+        "ref:vatin": "IT01238481004",
+        "re***REMOVED***aurant:type": "enoteca"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.489126,
+          41.8937195
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1688847325",
+      "properties": {
+        "@id": "node/1688847325",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "Gran Caffe Martini & Rossi"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4941649,
+          41.8897129
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1688847351",
+      "properties": {
+        "@id": "node/1688847351",
+        "addr:housenumber": "5",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "Al Gladiatore",
+        "phone": "+39 06 77209496"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.494046,
+          41.8894459
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1688847355",
+      "properties": {
+        "@id": "node/1688847355",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "Royal art café"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4942704,
+          41.8899551
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1731734008",
+      "properties": {
+        "@id": "node/1731734008",
+        "addr:city": "Roma",
+        "addr:country": "IT",
+        "addr:housenumber": "66",
+        "addr:po***REMOVED***code": "00185",
+        "addr:***REMOVED***reet": "Via Montebello",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "asian;pinoy;chinese",
+        "name": "Asian Delight",
+        "opening_hours": "Mo-Su 10:00-22:00; Sa 12:00-21:00",
+        "operator": "F. Cojeda Contado SNC",
+        "phone": "+39 06 48913216"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.5002433,
+          41.9055106
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1923912448",
+      "properties": {
+        "@id": "node/1923912448",
+        "addr:housenumber": "195",
+        "amenity": "re***REMOVED***aurant",
+        "contact:phone": "+39 06 4824888",
+        "drive_in": "no",
+        "name": "L'archetto di Cavour",
+        "wheelchair": "limited"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4935056,
+          41.8951179
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1926196447",
+      "properties": {
+        "@id": "node/1926196447",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "Fantasy"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.49949,
+          41.9045268
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1952358265",
+      "properties": {
+        "@id": "node/1952358265",
+        "addr:city": "Roma",
+        "addr:country": "IT",
+        "addr:housenumber": "44/46",
+        "addr:po***REMOVED***code": "00185",
+        "addr:***REMOVED***reet": "Via Tiburtina",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "pizza",
+        "name": "Pizzeria L'Economica",
+        "operator": "Lori Giulio & C. s.a.s.",
+        "oven": "wood_fired",
+        "ref:vatin": "IT0390762210001",
+        "re***REMOVED***aurant:type:it": "pizzeria"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.5127833,
+          41.8974467
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2612010515",
+      "properties": {
+        "@id": "node/2612010515",
+        "addr:city": "Roma",
+        "addr:housenumber": "21",
+        "addr:po***REMOVED***code": "00187",
+        "addr:***REMOVED***reet": "Via della Cordonata",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "regional",
+        "name": "Santa Cri***REMOVED***ina al Quirinale",
+        "phone": "+39 06 69925485",
+        "website": "http://www.ri***REMOVED***orantesantacri***REMOVED***inaalquirinale.it"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4864783,
+          41.8970834
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2735002326",
+      "properties": {
+        "@id": "node/2735002326",
+        "addr:housenumber": "315",
+        "amenity": "re***REMOVED***aurant",
+        "name": "Baires Imperiale",
+        "phone": "+39 06 69202164"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4888985,
+          41.8936579
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2735002356",
+      "properties": {
+        "@id": "node/2735002356",
+        "addr:housenumber": "27",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "pizza",
+        "name": "Tomoko Tudini",
+        "phone": "+39 06 4817586; +39 06 4818487"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4992448,
+          41.8994035
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2925136606",
+      "properties": {
+        "@id": "node/2925136606",
+        "addr:city": "Roma",
+        "addr:housenumber": "56",
+        "addr:po***REMOVED***code": "00187",
+        "addr:***REMOVED***reet": "Vicolo dei Modelli",
+        "amenity": "re***REMOVED***aurant",
+        "name": "La Fontana di Venere"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4844365,
+          41.900612
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2939415080",
+      "properties": {
+        "@id": "node/2939415080",
+        "addr:city": "Roma",
+        "addr:country": "IT",
+        "addr:housename": "La Taverna Italiana",
+        "addr:housenumber": "385",
+        "addr:po***REMOVED***code": "00185",
+        "addr:***REMOVED***reet": "Via Giovanni Giolitti",
+        "amenity": "re***REMOVED***aurant",
+        "name": "La Taverna Italiana"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.5109193,
+          41.8940946
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3052495271",
+      "properties": {
+        "@id": "node/3052495271",
+        "addr:city": "Roma",
+        "addr:housenumber": "24-24a",
+        "addr:***REMOVED***reet": "Via dei Santi Quattro",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "pizza",
+        "name": "Li Rioni"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4978334,
+          41.8886879
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3061568272",
+      "properties": {
+        "@id": "node/3061568272",
+        "addr:city": "Roma",
+        "addr:housenumber": "95",
+        "addr:po***REMOVED***code": "00186",
+        "addr:***REMOVED***reet": "Via in Arcione",
+        "amenity": "re***REMOVED***aurant",
+        "name": "Al Presidente"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4852166,
+          41.9015405
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3061568276",
+      "properties": {
+        "@id": "node/3061568276",
+        "addr:city": "Roma",
+        "addr:housenumber": "41",
+        "addr:po***REMOVED***code": "00186",
+        "addr:***REMOVED***reet": "Via del Lavatore",
+        "amenity": "re***REMOVED***aurant",
+        "name": "Birreria"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4840258,
+          41.9008905
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3061568277",
+      "properties": {
+        "@id": "node/3061568277",
+        "addr:city": "Roma",
+        "addr:housenumber": "34",
+        "addr:***REMOVED***reet": "Via del Lavatore",
+        "amenity": "re***REMOVED***aurant",
+        "name": "Da Cecere"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4844496,
+          41.9011041
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3101353149",
+      "properties": {
+        "@id": "node/3101353149",
+        "addr:city": "Roma",
+        "addr:housenumber": "9",
+        "addr:***REMOVED***reet": "Via della Madonna dei Monti",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "La Taverna dei Fori Imperiali"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4883814,
+          41.8939729
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3152274226",
+      "properties": {
+        "@id": "node/3152274226",
+        "addr:city": "Roma",
+        "addr:housenumber": "7",
+        "addr:po***REMOVED***code": "00185",
+        "addr:***REMOVED***reet": "Via Principe Amedeo",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "Trattoria il Gallo Nero"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4980494,
+          41.9000787
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3190236550",
+      "properties": {
+        "@id": "node/3190236550",
+        "addr:city": "Roma",
+        "addr:housenumber": "91",
+        "addr:***REMOVED***reet": "Via del Lavatore",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "pizza",
+        "name": "Piccolo Buco",
+        "phone": "+39 06 69380163"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4846406,
+          41.9013199
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3379569487",
+      "properties": {
+        "@id": "node/3379569487",
+        "addr:city": "Roma",
+        "addr:country": "IT",
+        "addr:housenumber": "54",
+        "addr:po***REMOVED***code": "00185",
+        "addr:***REMOVED***reet": "Via Varese",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "email": "info@meidinnapols.it",
+        "name": "Meid in Nepols",
+        "oven": "wood_fired",
+        "phone": "+39 06 44704131",
+        "website": "http://www.meidinnepols.com/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.5061437,
+          41.9023658
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3548494475",
+      "properties": {
+        "@id": "node/3548494475",
+        "addr:***REMOVED***reet": "San Martino dei Monte",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "La Forchetta di Oro",
+        "opening_hours": "mezze, serra",
+        "smoking": "no",
+        "wheelchair": "yes"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4988453,
+          41.8956182
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3554601726",
+      "properties": {
+        "@id": "node/3554601726",
+        "addr:city": "Roma",
+        "addr:country": "IT",
+        "addr:housenumber": "107",
+        "addr:po***REMOVED***code": "00184",
+        "addr:***REMOVED***reet": "Via Cavour",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "Ri***REMOVED***orante-Pizzeria Gallo Matto",
+        "outdoor_seating": "yes",
+        "phone": "+39 06 470354"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4954961,
+          41.8970481
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3554607395",
+      "properties": {
+        "@id": "node/3554607395",
+        "addr:city": "Roma",
+        "addr:country": "IT",
+        "addr:housenumber": "86",
+        "addr:po***REMOVED***code": "00184",
+        "addr:***REMOVED***reet": "Via Leonina",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "Ri***REMOVED***orante Ragno d'Oro",
+        "outdoor_seating": "yes",
+        "phone": "+39 06 4882003"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.492441,
+          41.8949731
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3575003304",
+      "properties": {
+        "@id": "node/3575003304",
+        "addr:city": "Roma",
+        "addr:housenumber": "10",
+        "addr:po***REMOVED***code": "00185",
+        "addr:***REMOVED***reet": "Via del Ca***REMOVED***ro Pretorio",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "japanese",
+        "name": "Hokkaido",
+        "opening_hours": "Mo-Su 11:00-15:00,18:00-24:00",
+        "phone": "+39 06 45505297"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.5046543,
+          41.9012469
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3658239261",
+      "properties": {
+        "@id": "node/3658239261",
+        "addr:city": "Roma",
+        "addr:housenumber": "58",
+        "addr:***REMOVED***reet": "Piazza dei Santi Apo***REMOVED***oli",
+        "amenity": "re***REMOVED***aurant",
+        "cuisine": "italian",
+        "name": "Bibo"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.4833635,
+          41.8974063
+        ]
+      }
+    }
+  ]
+};
