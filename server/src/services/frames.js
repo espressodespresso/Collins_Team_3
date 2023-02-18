f1d018ec11a81dc051deaed5797a06978a907d9a
[CS-4] <[cameron.smith2406@gmail.com]>
2023-02-18 11:30:15 +0000

    /api/scenes/:id gets all frames for a scene

--- server/src/services/frames.js
+++ server/src/services/frames.js
@@ -1,18 +1,31 @@
 import { sendGET } from './apiReque***REMOVED***.js'
 import config from '../config/index.js'
+import {nodeCache} from '../db.js'
 
 con***REMOVED*** getFrames = async(req, res) => {
-    con***REMOVED*** url = req.body.producturl
-    con***REMOVED*** apiRes = await sendGET(url, config.acces***REMOVED***oken)
-    con***REMOVED*** frames = apiRes.scenes.bands.frames
+    let producturl = nodeCache.get(req.params.id) 
+
+    if(producturl === undefined){
+        con***REMOVED*** apiRes = await sendGET(`https://hallam.***REMOVED***.com/discover/api/v1/products/${req.params.id}`, req.accessToken)
+        producturl = apiRes.product.result.producturl
+    }
+
+    con***REMOVED*** apiRes = await sendGET(producturl, req.accessToken)
+    con***REMOVED*** frames = apiRes.scenes[0].bands[0].frames
+    con***REMOVED*** frameRes = []
 
     for(let i = frames.length-1; --i > -1;){
-        con***REMOVED*** url = `https://hallam.***REMOVED***.com/discover/api/v1/products/${frames[i].id}`
-        con***REMOVED*** apiRes = await sendGET(url, config.acces***REMOVED***oken)
+        con***REMOVED*** url = `https://hallam.***REMOVED***.com/discover/api/v1/products/${frames[i].productId}`
+        con***REMOVED*** apiRes = await sendGET(url, req.accessToken)
         con***REMOVED*** frameData = apiRes.product.result
 
-        
+        frameRes.push({
+            title: frameData.title,
+            footprint: frameData.footprint,
+        })
     }
+
+    res.json({data: frameRes})
 }
 
 export{getFrames}
