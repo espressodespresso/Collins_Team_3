1ea3b739d7753f6f5016bc45fc6cd6b190ef2f89
[CS-4] <[cameron.smith2406@gmail.com]>
2023-02-18 11:30:15 +0000

    /api/scenes/:id gets all frames for a scene

--- server/src/services/frames.js
+++ server/src/services/frames.js
@@ -1,18 +1,31 @@
 import { sendGET } from './apiRequest.js'
 import config from '../config/index.js'
+import {nodeCache} from '../db.js'
 
 const getFrames = async(req, res) => {
-    const url = req.body.producturl
-    const apiRes = await sendGET(url, config.accesstoken)
-    const frames = apiRes.scenes.bands.frames
+    let producturl = nodeCache.get(req.params.id) 
+
+    if(producturl === undefined){
+        const apiRes = await sendGET(`https://hallam.sci-toolset.com/discover/api/v1/products/${req.params.id}`, req.accessToken)
+        producturl = apiRes.product.result.producturl
+    }
+
+    const apiRes = await sendGET(producturl, req.accessToken)
+    const frames = apiRes.scenes[0].bands[0].frames
+    const frameRes = []
 
     for(let i = frames.length-1; --i > -1;){
-        const url = `https://hallam.sci-toolset.com/discover/api/v1/products/${frames[i].id}`
-        const apiRes = await sendGET(url, config.accesstoken)
+        const url = `https://hallam.sci-toolset.com/discover/api/v1/products/${frames[i].productId}`
+        const apiRes = await sendGET(url, req.accessToken)
         const frameData = apiRes.product.result
 
-        
+        frameRes.push({
+            title: frameData.title,
+            footprint: frameData.footprint,
+        })
     }
+
+    res.json({data: frameRes})
 }
 
 export{getFrames}
