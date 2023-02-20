971e31fcbf17dc87ca6b1a17309930a6f44bd3b7
[CS-4] <[cameron.smith2406@gmail.com]>
2023-02-20 13:42:18 +0000

    Added network util that returns status code with response data, updated route handlers to use network util

--- server/src/services/frames.js
+++ server/src/services/frames.js
@@ -1,31 +1,64 @@
-import { sendGET } from './apiRequest.js'
 import config from '../config/index.js'
 import {nodeCache} from '../db.js'
+import network from '../utils/network.js'
 
 const getFrames = async(req, res) => {
     let producturl = nodeCache.get(req.params.id) 
 
     if(producturl === undefined){
-        const apiRes = await sendGET(`https://hallam.sci-toolset.com/discover/api/v1/products/${req.params.id}`, req.accessToken)
-        producturl = apiRes.product.result.producturl
+        try{
+            const url = `https://hallam.sci-toolset.com/discover/api/v1/products/${req.params.id}`
+            const auth = `Bearer ${encodeURI(req.accessToken)}`
+            const headers = {
+                "Content-Type": "application/json",
+                "Authorization": auth,
+                "Accept": "*/*"
+            }
+            const apiRes = await network.get(url, headers)
+            producturl = apiRes.data.product.result.producturl
+        }catch(e){
+            console.error(e)
+            res.status(500).json({message: "Internal Server Error"})
+        }
+
+        try{
+        const auth = `Bearer ${encodeURI(req.accessToken)}`
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": auth,
+            "Accept": "*/*"
+        }
+
+        const apiRes = await network.get(producturl, headers)
+
+        if(apiRes.status === 200){
+            const frames = apiRes.data.scenes[0].bands[0].frames
+            const frameRes = []
+    
+            for(let i = frames.length-1; --i > -1;){
+                const url = `https://hallam.sci-toolset.com/discover/api/v1/products/${frames[i].productId}`
+                const auth = `Bearer ${encodeURI(req.accessToken)}`
+                const headers = {
+                    "Content-Type": "application/json",
+                    "Authorization": auth,
+                    "Accept": "*/*"
+                }
+                const apiRes = await network.get(url, headers)
+                const frameData = apiRes.data.product.result
+        
+                frameRes.push({
+                    title: frameData.title,
+                    footprint: frameData.footprint,
+                })
+            }
+            res.json({data: frameRes})
+        }else{
+            res.status(500).json({message: "Internal Server Error"})
+        }}catch(e){
+        console.error(e)
+        res.status(500).json({message: "Internal Server Error"})
+        }
     }
-
-    const apiRes = await sendGET(producturl, req.accessToken)
-    const frames = apiRes.scenes[0].bands[0].frames
-    const frameRes = []
-
-    for(let i = frames.length-1; --i > -1;){
-        const url = `https://hallam.sci-toolset.com/discover/api/v1/products/${frames[i].productId}`
-        const apiRes = await sendGET(url, req.accessToken)
-        const frameData = apiRes.product.result
-
-        frameRes.push({
-            title: frameData.title,
-            footprint: frameData.footprint,
-        })
-    }
-
-    res.json({data: frameRes})
 }
 
 export{getFrames}
--- server/src/services/missions.js
+++ server/src/services/missions.js
@@ -1,19 +1,31 @@
-import { sendGET } from './apiRequest.js'
 import {nodeCache} from '../db.js'
-import config from '../config/index.js'
+import network from '../utils/network.js'
 
 const getMissions = async (req, res) => {
+
     const url = `https://hallam.sci-toolset.com/discover/api/v1/missionfeed/missions/`
-    const apiRes = await sendGET(url, req.accessToken)
+    const auth = `Bearer ${encodeURI(req.accessToken)}`
+    const headers = {
+        "Content-Type": "application/json",
+        "Authorization": auth,
+        "Accept": "*/*"
+    }
+    
+    try{
+        const apiRes = await network.get(url, headers)
 
-    if(apiRes){
-        const userMissions = apiRes.missions
-        res.json({data: userMissions})
-
-        for(let i = userMissions.length; --i > -1;){
-            cacheScenes(userMissions[i].id, req.accessToken)
+        if(apiRes.status === 200){
+            const userMissions = apiRes.data.missions
+            res.json({data: userMissions})
+    
+            for(let i = userMissions.length; --i > -1;){
+                cacheScenes(userMissions[i].id, req.accessToken)
+            }
+         }else{
+            res.status(500).json({message: "Internal Server Error"})
         }
-     }else{
+    }catch(e){
+        console.error(e)
         res.status(500).json({message: "Internal Server Error"})
     }
 
@@ -21,55 +33,108 @@ const getMissions = async (req, res) => {
 
 const getMissionScenes = async (req, res) => {
 
-    if(nodeCache.get(req.params.id) === undefined){
-        const url = `https://hallam.sci-toolset.com/discover/api/v1/missionfeed/missions/${req.params.id}`
-        const apiRes = await sendGET(url, req.accessToken)
-
-        if(apiRes){
-            const scenes = apiRes.scenes
-    
-            for(let i = scenes.length; --i > -1;){
-                const url = `https://hallam.sci-toolset.com/discover/api/v1/products/${scenes[i].id}`
-                const apiRes = await sendGET(url, req.accessToken)
-                const sceneData = apiRes.product.result
-    
-                delete scenes[i].bands
-                
-                scenes[i].name = sceneData.title
-                scenes[i].countrycode = sceneData.countrycode
-                scenes[i].centre = sceneData.centre
-                scenes[i].footprint = sceneData.footprint
-                scenes[i].producturl = sceneData.producturl
-
-                nodeCache.set(scenes[i].id, scenes[i].producturl)
-            }
-            res.json({data: scenes})
-        }else{
-            res.status(500).json({message: "Internal Server Error"})
-        }
-    }else{
+    if(nodeCache.get(req.params.id) !== undefined){
         res.json({data: nodeCache.get(req.params.id)})
+    }else{
+        try{
+            const url = `https://hallam.sci-toolset.com/discover/api/v1/missionfeed/missions/${req.params.id}`
+            const auth = `Bearer ${encodeURI(req.accessToken)}`
+            const headers = {
+                "Content-Type": "application/json",
+                "Authorization": auth,
+                "Accept": "*/*"
+            }
+    
+            const apiRes = await network.get(url, headers)
+    
+            if(apiRes.status === 200){
+                const scenes = apiRes.data.scenes
+        
+                for(let i = scenes.length; --i > -1;){
+                    const url = `https://hallam.sci-toolset.com/discover/api/v1/products/${scenes[i].id}`
+                    const auth = `Bearer ${encodeURI(req.accessToken)}`
+                    const headers = {
+                        "Content-Type": "application/json",
+                        "Authorization": auth,
+                        "Accept": "*/*"
+                    }
+    
+                    const apiRes = await network.get(url, headers)
+    
+                    if(apiRes.status === 200){
+                        const sceneData = apiRes.data.product.result
+        
+                        delete scenes[i].bands
+                    
+                        scenes[i].name = sceneData.title
+                        scenes[i].countrycode = sceneData.countrycode
+                        scenes[i].centre = sceneData.centre
+                        scenes[i].footprint = sceneData.footprint
+                        scenes[i].producturl = sceneData.producturl
+    
+                        nodeCache.set(scenes[i].id, scenes[i].producturl)
+                    }else{
+                        throw error("Couldn't get all scenes")
+                    }
+                }
+                res.json({data: scenes})
+            }
+            else if(apiRes.status === 404){
+                res.status(404).json({message: "Mission Not Found"})
+            }
+            else{
+                res.status(500).json({message: "Internal Server Error"})
+            }
+        }catch(e){
+            console.error(e)
+            res.status(500).json({message: "Internal Sever Error"})
+        }
     }
 }
 
     const cacheScenes = async (id, accessToken) => {
         const url = `https://hallam.sci-toolset.com/discover/api/v1/missionfeed/missions/${id}`
-        const apiRes = await sendGET(url, accessToken)
-        const scenes = apiRes.scenes
-
-        for(let i = scenes.length; --i > -1;){
-            const url = `https://hallam.sci-toolset.com/discover/api/v1/products/${scenes[i].id}`
-            const apiRes = await sendGET(url, accessToken)
-            const sceneData = apiRes.product.result
-
-            delete scenes[i].bands
-
-            scenes[i].countrycode = sceneData.countrycode
-            scenes[i].centre = sceneData.centre
-            scenes[i].footprint = sceneData.footprint
+        const auth = `Bearer ${encodeURI(accessToken)}`
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": auth,
+            "Accept": "*/*"
         }
-        nodeCache.set(id, scenes)
+
+        try{
+            const apiRes = await network.get(url, headers)
+
+            if(apiRes.status === 200){
+                const scenes = apiRes.data.scenes
+
+                for(let i = scenes.length; --i > -1;){
+                    const url = `https://hallam.sci-toolset.com/discover/api/v1/products/${scenes[i].id}`
+                    const auth = `Bearer ${encodeURI(accessToken)}`
+                    const headers = {
+                        "Content-Type": "application/json",
+                        "Authorization": auth,
+                        "Accept": "*/*"
+                    }
+                    const apiRes = await network.get(url, headers)
+                    const sceneData = apiRes.data.product.result
+        
+                    delete scenes[i].bands
+        
+                    scenes[i].countrycode = sceneData.countrycode
+                    scenes[i].centre = sceneData.centre
+                    scenes[i].footprint = sceneData.footprint
+                }
+                nodeCache.set(id, scenes)
+            }else{
+                return null
+            }
+        }catch(e){
+            console.error(e)
+            return null
+        }
     }
+
+        
 
 
 export {getMissions, getMissionScenes}
