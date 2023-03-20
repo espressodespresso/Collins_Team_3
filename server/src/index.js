6fc2797d47963f5981f741410e3686256c9fc2a4
[CS-4] <[cameron.smith2406@gmail.com]>
2023-03-20 13:52:47 +0000

    HTTP2 can be enabled by setting environment variable HTTP2_ENBALED to 'true', ssl-certs must be generated beforehand using the npm generate-cert script

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,35 @@
 import app from './server.js'
+import spdy from 'spdy'
 import * as dotenv from 'dotenv'
+import fs from 'fs'
 
 dotenv.config()
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server started on http://localhost:${process.env.PORT}`)
-})
+const CERT_DIR = new URL('../cert', import.meta.url).pathname
+const HTTP2_ENABLED = process.env.HTTP2_ENABLED
+
+const options =   {
+  key: fs.readFileSync(`${CERT_DIR}/server.key`),
+  cert: fs.readFileSync(`${CERT_DIR}/server.cert`),
+}
+
+const createServer = () => {
+  if(HTTP2_ENABLED == 'false') {
+    return app;
+  }else{
+    return spdy.createServer(
+      {
+        key: fs.readFileSync(`${CERT_DIR}/server.key`),
+        cert: fs.readFileSync(`${CERT_DIR}/server.cert`),
+      },
+      app
+    );
+  }
+}
+
+const server = createServer();
+
+server.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}`);
+  console.log(`HTTP2: ${HTTP2_ENABLED=='true' ? 'enabled' : 'disabled'}`);
+});
