4ce72b5fd7f1665a2f94458c0538907f08e9a5c0
[CS-4] <[cameron.smith2406@gmail.com]>
2023-03-25 13:44:03 +0000

    userModel handles caching and retrieval of user api tokens, authService handles logins, redisClient is a facade for redisclient, tempCache renamed from db.js

---
+++ server/src/modules/redisClient.js
@@ -0,0 +1,17 @@
+import { json } from "express"
+
+export default class RedisClient{
+    con***REMOVED***ructor(client){
+        this.client = client
+    }
+
+    async getJSON(key){
+        con***REMOVED*** jsonString = await this.client.get(key)
+        return JSON.parse(jsonString)
+    }
+
+    async setJSON(key, jsonValue){
+        con***REMOVED*** value = JSON.***REMOVED***ringify(jsonValue)
+        return await this.client.set(key, value)
+    }
+}
