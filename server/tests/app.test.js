b8f0ec57d367fdf157bde7dcd18fffe344b873d3
[CS-4] <[cameron.smith2406@gmail.com]>
2023-03-27 14:49:34 +0100

    integration tests written

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -34,5 +34,109 @@ describe("/login", () => {
         expect(responseInvalidPassword._body).toHaveProperty("message", errmsg)
         expect(responseBodyInvalid._body).toHaveProperty("message", errmsg)
     })
+
+    te***REMOVED***("/POST 400 response sent if invalid json body provided", async () => {
+        con***REMOVED*** bodyInvalidPassword = {username: config.username, pass: config.password}
+        con***REMOVED*** bodyInvalidUsername = {user: config.username, password: config.password}
+        con***REMOVED*** bodyInvalid = {user: config.username, pass: config.password}
+        con***REMOVED*** responseInvalidPassword = await reque***REMOVED***(app).po***REMOVED***("/login").send(bodyInvalidPassword)
+        con***REMOVED*** responseInvalidUsername = await reque***REMOVED***(app).po***REMOVED***("/login").send(bodyInvalidUsername)
+        con***REMOVED*** responseInvalid = await reque***REMOVED***(app).po***REMOVED***("/login").send(bodyInvalid)
+        expect(responseInvalidPassword.***REMOVED***atusCode).toBe(400)
+        expect(responseInvalidUsername.***REMOVED***atusCode).toBe(400)
+        expect(responseInvalid.***REMOVED***atusCode).toBe(400)
+    })
 })
 
+describe("/api", () => {
+
+    let jwt = undefined
+
+    beforeAll(async () => {
+        con***REMOVED*** body = {username: config.username, password: config.password}
+        con***REMOVED*** response = await reque***REMOVED***(app).po***REMOVED***("/login").send(body)
+        jwt = response._body.token
+    })
+
+    te***REMOVED***("Cannot access /api routes with no jwt", async () => {
+        con***REMOVED*** response = await reque***REMOVED***(app).get("/api/products/scenes")
+        expect(response.***REMOVED***atusCode).toBe(401)
+    })
+
+    te***REMOVED***("Cannot access /api routes with invalid jwt", async () => {
+        con***REMOVED*** response = await reque***REMOVED***(app).get("/api/products/scenes").set('Authorization', `Bearer invalidjwt`)
+        expect(response.***REMOVED***atusCode).toBe(401)
+    })
+
+    describe("/api/products/scenes", () => {
+        te***REMOVED***("/GET 200 response sends a li***REMOVED*** of product IDs", async () => {
+            con***REMOVED*** response = await reque***REMOVED***(app).get("/api/products/scenes").set('Authorization', `Bearer ${jwt}`)
+            expect(response.***REMOVED***atusCode).toBe(200)
+            expect(response._body.data.every(e => e.hasOwnProperty('id'))).toBe(true)
+        })
+    })
+
+    describe("/api/products", () => {
+        te***REMOVED***("/POST 200 response sends a li***REMOVED*** of products", async () => {
+            con***REMOVED*** productIds = [
+                "7dcecde2-391c-4f1d-b017-769bdbf587a8", 
+                "e9992195-cebc-4cdf-9899-4b7e43d03392",
+                "fed54abc-ae5e-44f4-bc8b-c7c431275cb7",
+                "e8d2fe6c-4c1b-4748-9ae8-041d463afcbe",
+                "9393bd47-6359-4374-b696-ecc7bbf56982",
+                "7f8f1ba3-17ec-43b5-ad51-ccca360e265c",
+            ]
+            con***REMOVED*** body = {products: productIds}
+            con***REMOVED*** response = await reque***REMOVED***(app).po***REMOVED***("/api/products").set('Authorization', `Bearer ${jwt}`).send(body)
+            expect(response.***REMOVED***atusCode).toBe(200)
+            expect(response._body.data.every(e => e.hasOwnProperty('product'))).toBe(true)
+        })
+
+        te***REMOVED***("/POST 400 response sent if a li***REMOVED*** of invalid products is provided", async () => {
+            con***REMOVED*** productIds = [
+                "7dcecde2-391c-4f1d-b017-769bdbinvalid", 
+                "e9992195-cebc-4cdf-9899-4b7e43invalid",
+                "fed54abc-ae5e-44f4-bc8b-c7c431invalid",
+                "e8d2fe6c-4c1b-4748-9ae8-041d46invalid",
+                "9393bd47-6359-4374-b696-ecc7bbinvalid",
+                "7f8f1ba3-17ec-43b5-ad51-ccca36invalid",
+            ]
+            con***REMOVED*** body = {products: productIds}
+            con***REMOVED*** response = await reque***REMOVED***(app).po***REMOVED***("/api/products").set('Authorization', `Bearer ${jwt}`).send(body)
+            expect(response.***REMOVED***atusCode).toBe(400)
+            expect(response._body.data).toHaveProperty("message")
+        })
+
+        te***REMOVED***("/POST 400 response sent if a li***REMOVED*** of valid and invalid products is provided", async () => {
+            con***REMOVED*** productIds = [
+                "7dcecde2-391c-4f1d-b017-769bdbf587a8", 
+                "e9992195-cebc-4cdf-9899-4b7e4invalid",
+                "fed54abc-ae5e-44f4-bc8b-c7c431275cb7",
+                "e8d2fe6c-4c1b-4748-9ae8-041d4invalid",
+                "9393bd47-6359-4374-b696-ecc7bbf56982",
+                "7f8f1ba3-17ec-43b5-ad51-ccca3invalid",
+            ]
+            con***REMOVED*** body = {products: productIds}
+            con***REMOVED*** response = await reque***REMOVED***(app).po***REMOVED***("/api/products").set('Authorization', `Bearer ${jwt}`).send(body) 
+            expect(response.***REMOVED***atusCode).toBe(400)
+            expect(response._body.data).toHaveProperty("message")
+        })
+
+        te***REMOVED***("/POST 404 response sent if an empty li***REMOVED*** of products is provided", async () => {
+            con***REMOVED*** body = {products: []}
+            con***REMOVED*** response = await reque***REMOVED***(app).po***REMOVED***("/api/products").set('Authorization', `Bearer ${jwt}`).send(body) 
+            expect(response.***REMOVED***atusCode).toBe(404)
+            expect(response._body.data).toHaveProperty("message")
+        })
+
+        te***REMOVED***("/POST 400 response sent if an invalid json body is provided", async () => {
+            con***REMOVED*** bodyInvalidField = {prodIds: []}
+            con***REMOVED*** bodyInvalidValue = {products: "7dcecde2-391c-4f1d-b017-769bdbf587a8"}
+            con***REMOVED*** responseInvalidField = await reque***REMOVED***(app).po***REMOVED***("/api/products").set('Authorization', `Bearer ${jwt}`).send(bodyInvalidField) 
+            con***REMOVED*** responseInvalidValue = await reque***REMOVED***(app).po***REMOVED***("/api/products").set('Authorization', `Bearer ${jwt}`).send(bodyInvalidValue) 
+            expect(responseInvalidField.***REMOVED***atusCode).toBe(400)
+            expect(responseInvalidValue.***REMOVED***atusCode).toBe(400)
+        })
+    })
+    
+})
