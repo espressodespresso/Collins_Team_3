fe0782882899c4c4c357dc64c15c1f5a64dce3c1
[CS-4] <[cameron.smith2406@gmail.com]>
2023-03-27 10:28:45 +0100

    Auth Service debugged and unit tests written

--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -8,11 +8,13 @@ export default class AuthService{
 
     async login(username, password){
        const status = await this.userModel.signIn(username, password)
-       const result = {jwt: undefined}
+       let result = {jwt: undefined}
        if(status === true){
+            result.status = 200
             result.jwt = createJWT({username})
+       }else{
+            result = {status: 400, data: {message: "invalid username and password combination"}}
        }
        return result
     }
-
 }
