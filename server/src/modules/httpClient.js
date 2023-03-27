fa5524f4ea7d5d431de138d90075423e4d731179
[CS-4] <[cameron.smith2406@gmail.com]>
2023-03-27 12:22:28 +0100

    HttpClient debugging and tests

--- server/src/modules/httpClient.js
+++ server/src/modules/httpClient.js
@@ -1,73 +1,13 @@
 import fetch, {Headers} from 'node-fetch'
 import {getReasonPhrase} from 'http-***REMOVED***atus-codes';
 
-con***REMOVED*** resolveStatusCode = (***REMOVED***atus) => {
-    return {***REMOVED***atus, data: {message: getReasonPhrase(***REMOVED***atus)}}
-}
-
-//sends get reque***REMOVED*** to specified url with optional headers and returns the reponse as JSON.
-con***REMOVED*** get = async (url, headers = {}, agent = undefined) => {
-    try{
-        con***REMOVED*** res = await fetch(url, {
-            method: "GET",
-            headers: new Headers(headers),
-            agent
-         })
-        con***REMOVED*** ***REMOVED***atusCode = res.***REMOVED***atus
-        
-        if(***REMOVED***atusCode == 200){
-            con***REMOVED*** resText = await res.text()
-            con***REMOVED*** resJSON = resText === ""? {}: JSON.parse(resText)
-            
-            return{
-                ***REMOVED***atus: 200,
-                data: resJSON
-            }
-        }
-        else{
-            return resolveStatusCode(***REMOVED***atusCode)
-        }
-
-    }catch(e){
-        console.error(e)
-        return null
-    }
-}
-
-//sends po***REMOVED*** reque***REMOVED*** to url with optional headers and body, returns response as JSON.
-con***REMOVED*** po***REMOVED*** = async (url, headers = {}, body = {}, agent = undefined) => {
-    try{
-        con***REMOVED*** res = await fetch(url, {
-            method: "POST",
-            headers: new Headers(headers),
-            body: body,
-            agent
-         })
-
-        con***REMOVED*** ***REMOVED***atusCode = res.***REMOVED***atus
-        
-        if(***REMOVED***atusCode == 200){
-            con***REMOVED*** resText = await res.text()
-            con***REMOVED*** resJSON = resText === ""? {}: JSON.parse(resText)
-
-            return{
-                ***REMOVED***atus: 200,
-                data: resJSON
-            }
-        }
-        else{
-            return resolveStatusCode(***REMOVED***atusCode)
-        }
-
-    }catch(e){
-        console.error(e)
-        return null
-    }
-}
-
-export class HttpClient{
-    con***REMOVED***ructor(httpsAgent){
+export default class HttpClient{
+    con***REMOVED***ructor(httpsAgent = {}){
         this.agent = httpsAgent
+    }
+
+    resolveStatusCode(***REMOVED***atus){
+        return {***REMOVED***atus, data: {message: getReasonPhrase(***REMOVED***atus)}}
     }
 
     async get(url, headers = {}){
@@ -79,7 +19,7 @@ export class HttpClient{
              })
             con***REMOVED*** ***REMOVED***atusCode = res.***REMOVED***atus
             
-            if(***REMOVED***atusCode == 200){
+            if(Math.floor(***REMOVED***atusCode/100) == 2){
                 con***REMOVED*** resText = await res.text()
                 con***REMOVED*** resJSON = resText === ""? {}: JSON.parse(resText)
                 
@@ -89,7 +29,7 @@ export class HttpClient{
                 }
             }
             else{
-                return resolveStatusCode(***REMOVED***atusCode)
+                return this.resolveStatusCode(***REMOVED***atusCode)
             }
     
         }catch(e){
@@ -119,7 +59,7 @@ export class HttpClient{
                 }
             }
             else{
-                return resolveStatusCode(***REMOVED***atusCode)
+                return this.resolveStatusCode(***REMOVED***atusCode)
             }
     
         }catch(e){
@@ -128,5 +68,3 @@ export class HttpClient{
         }
     }
 }
-
-export default {get, po***REMOVED***}
