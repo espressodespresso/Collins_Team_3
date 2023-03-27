d96278b16cedd624aa1039ba303e40df4a50e3ab
Harry <harry.martel@hotmail.com>
2023-03-27 04:41:51 +0100

    Fixed compile errors, improved script performance, added npm scripts

---
+++ client/scripts/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo "Attempting to build..."
+
+{
+  tsc && echo "Compiling typescript..." &&
+  browserify ../src/js/index.js -o ../src/js/index.js && echo "Translating using browserify..." &&
+  uglifyjs ../src/js/index.js -mc > ../src/js/index.min.js && echo "Minifying using uglifyjs..." &&
+  echo "Success..."
+} || {
+  echo "Build failed... ($__EXCEPTION_SOURCE__)"
+  sleep 10s
+}
