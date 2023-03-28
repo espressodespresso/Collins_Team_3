431556f9151e5738bfa580f7ca4ef1e31c586983
Harry <harry.martel@hotmail.com>
2023-03-28 04:54:27 +0100

    Added limited geocoder support

--- client/src/js/index.ts
+++ client/src/js/index.ts
@@ -1,9 +1,9 @@
-import {Map, TileLayer, FeatureGroup, Draw, Control, Polygon, GeoJSON} from "leaflet";
+import {Map, TileLayer, FeatureGroup, Control} from "leaflet";
 import "leaflet-draw";
 import {verifyCred} from "./services/auth";
 import {getMissions, Mission, MissionLayerGroup} from "./mission";
-import {FormatSidebar} from "./sidebar";
-import {addLayersToMap, calculateDrawnCoverage, clearMapLayers, generateLayers, initLayers, Levels} from "./map";
+import {FormatSidebar, initSearchEvent} from "./sidebar";
+import {addLayersToMap, initDrawEvent, initLayers, initZoomEvent, Levels} from "./map";
 import {polygon} from "@turf/helpers";
 
 let missions: Mission[] = [];
@@ -29,23 +29,6 @@ map.addControl(new Control.Draw({
 let drawFeatures = new FeatureGroup();
 map.addLayer(drawFeatures);
 
-map.on(Draw.Event.CREATED, async function (e) {
-    let layer = e.layer;
-    let drawGeoJSON = layer.toGeoJSON();
-    switch (drawGeoJSON.geometry.type) {
-        case "Polygon":
-            let calculatedFeature: GeoJSON.Feature = await calculateDrawnCoverage(polygon(drawGeoJSON.geometry.coordinates), missions);
-            let calculatedLayer = new GeoJSON(calculatedFeature);
-            calculatedLayer.setStyle({color: '#ffc107'})
-            await clearMapLayers(map);
-            drawFeatures.addLayer(calculatedLayer);
-            layer.setStyle({color: '#5c5c5c'})
-            drawFeatures.addLayer(layer);
-            break;
-    }
-})
-
-
 /*(L.Control as any).geocoder({
     defaultMarkCode: true
 }).addTo(map);*/
@@ -69,27 +52,16 @@ async function ***REMOVED***art(): Promise<void> {
         }).catch(e => console.error(e));
 }
 
-function loaded() {
+async function loaded() {
     let spinner = document.getElementById("spinner-container");
     spinner.classLi***REMOVED***.add("invisible");
     level = Levels.Marker;
+    /*let te***REMOVED***:IGeocoder;
+    let te***REMOVED***2:GeocodingCallback;
+    te***REMOVED***.geocode("Sheffield", te***REMOVED***2);*/
+    initZoomEvent(map, level, missions);
+    initDrawEvent(map, drawFeatures, missions);
+    //await initSearchEvent();
 }
-
-map.on("zoomend", async function (e) {
-    let zoomLevel = map.getZoom();
-    if(zoomLevel > 6 && zoomLevel < 10 && level !== Levels.Footprint) {
-        level = Levels.Footprint;
-        let generatedLayers = await generateLayers(missions, level);
-        addLayersToMap(generatedLayers, true, map);
-    } else if(zoomLevel > 9 && level !== Levels.Frame) {
-        level = Levels.Frame;
-        let generatedLayers = await generateLayers(missions, level);
-        addLayersToMap(generatedLayers, true, map);
-    } else if(zoomLevel < 7 && level !== Levels.Marker) {
-        level = Levels.Marker;
-        let generatedLayers = await generateLayers(missions, level);
-        addLayersToMap(generatedLayers, true, map);
-    }
-})
 
 ***REMOVED***art();
