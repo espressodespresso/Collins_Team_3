22bb0b43b2fe6237e04c8b4543d91e4dc0f3d665
[CS-4] <[cameron.smith2406@gmail.com]>
2023-03-30 11:35:05 +0100

    Switched over to localCache for development

--- server/src/loaders/index.js
+++ server/src/loaders/index.js
@@ -1,6 +1,7 @@
 import app from './express.js'
 import redisClientLoader from './redisClient.js'
-import dependencyInjectorLoader from './dependencyInjector.js'
+import LocalCache from '../../te***REMOVED***s/setup/localCache.js'
+import dependencyInjectorLoader from '../../te***REMOVED***s/setup/dependencyInjector.js'
 import UserModel from '../models/userModel.js'
 import ProductModelFactory from '../models/productModel.js'
 import ProductSearchBuilder from '../models/ProductSearch.js'
@@ -12,7 +13,7 @@ import MissionServiceFactory from '../services/missionService.js'
 
 export con***REMOVED*** createApp = async () => {
 
-    con***REMOVED*** redisClient = await redisClientLoader()
+    con***REMOVED*** localCache = new LocalCache()
 
     con***REMOVED*** userModel = {
         name: "User",
@@ -52,7 +53,7 @@ export con***REMOVED*** createApp = async () => {
     con***REMOVED*** models = [userModel, productModelFactory, productSearchBuilder, missionModelFactory]
     con***REMOVED*** services = [authService, productServiceFactory, missionServiceFactory]
 
-    dependencyInjectorLoader(redisClient, models, services)
+    dependencyInjectorLoader(localCache, models, services)
     
     return app
 }
